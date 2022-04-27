4b628f7687021b082520302b5c3b19fa941b395c
sowinskipatryk <91700001+sowinskipatryk@users.noreply.github.com>
2022-04-27 20:30:15 +0200

    added sqlite3 db

--- screens/loginscreen.py
+++ screens/loginscreen.py
@@ -1,18 +1,25 @@
 from kivy.uix.screenmanager import Screen
 from kivymd.uix.screen import MDScreen
+import sqlite3
 
 class LoginScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+
     def validate_pass(self):
         if not self.ids.loginpass_field.text:
             self.ids.info_label.text = "Empty input field!"
-        elif self.ids.loginpass_field.text != '1234':
-            self.ids.info_label.text = "Wrong login password!"
-            self.ids.loginpass_field.text = ""
         else:
-            self.manager.current = "passcheck"
+            conn = sqlite3.connect('vault.db')
+            c = conn.cursor()
+            c.execute("SELECT * FROM admin")
+            if self.ids.loginpass_field.text != c.fetchall()[0][0]:
+                self.ids.info_label.text = "Wrong login password!"
+            else:
+                self.manager.current = "passcheck"
+            conn.commit()
+            conn.close()
 
     def toggle_eye(self):
         if self.ids.eye_icon.icon == 'eye-off':
